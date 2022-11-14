a9913446aa6d211732de6041f71268415f0ecd55
KatOli4ka <katoli4ka87@mail.ru>
2022-11-14 19:38:43 +0500

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание 1");
+        System.out.println("Задача 1");
+
+         int [] arr=new int[3];
+         arr[0]=1;
+         arr[1]=2;
+         arr[2]=3;
+         for (int i=0; i<3; i++)
+        System.out.println(arr[i]);
+
+        System.out.println();
+        double[] arr1= {1.57, 7.654, 9.986};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arr1[i]);
+        }
+        System.out.println();
+        int [] arr2 = {4,6,9,3,0,11};
+        for (int i = 0; i < 6; i++) {
+            System.out.println(arr2[i]);
+        }
+
+
+
+
+
+
+
 
     }
 }
